fb1a89848a4f8e716022bc4d0ec87245199cc53e
wcx080805 <2867371170@qq.com>
2023-02-25 19:42:47 +0800

    nothing

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-const page = document.querySelector('.page');
-document.addEventListener('keyup', event => {
-  if (event.keyCode === 39) { // 按下右箭头键
-    page.classList.toggle('active');
-  }
+const pages = document.querySelectorAll('.page');
+const home = document.querySelector('#home');
+const page2 = document.querySelector('#page2');
+const arrow = document.querySelector('.arrow');
+
+// 点击箭头按钮跳转到第二页
+arrow.addEventListener('click', () => {
+  home.classList.remove('active');
+  page2.classList.add('active');
+});
+
+// 切换页面时添加或删除 active 类
+pages.forEach(page => {
+  page.addEventListener('transitionend', () => {
+    if (page.classList.contains('active')) {
+      page.style.zIndex = 1;
+    } else {
+      page.style.zIndex = -1;
+    }
+  });
 });
