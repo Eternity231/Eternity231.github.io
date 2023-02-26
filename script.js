a9acc0766ce6e1774f2f826c9058e148e19e2460
wcx080805 <2867371170@qq.com>
2023-02-26 20:22:42 +0800

    again

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const pages = document.querySelectorAll('.page');
 const home = document.querySelector('#home');
+const page1 = document.querySelector('#page1');
 const page2 = document.querySelector('#page2');
 const arrow = document.querySelector('.arrow');
-const prevButton = document.querySelector('#prev');
-const nextButton = document.querySelector('#next');
+const nextPage = document.querySelector('.next-page');
+const prevPage = document.querySelector('.prev-page');
+const page3 = document.querySelector('#page3');
 
-// 点击箭头按钮跳转到第二页
 arrow.addEventListener('click', () => {
   home.classList.remove('active');
-  page2.classList.add('active');
+  page1.classList.add('active');
 });
 
-// 切换页面时添加或删除 active 类
 pages.forEach(page => {
   page.addEventListener('transitionend', () => {
     if (page.classList.contains('active')) {
@@ -20,15 +20,4 @@ pages.forEach(page => {
       page.style.zIndex = -1;
     }
   });
-});
-
-// 点击 Prev 按钮回到第一页
-prevButton.addEventListener('click', () => {
-  page2.classList.remove('active');
-  home.classList.add('active');
-});
-
-// 点击 Next 按钮跳转到第三页（示例）
-nextButton.addEventListener('click', () => {
-  alert('Go to Page 3'); // 替换为跳转代码
 });
